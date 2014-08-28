7098b523e719127ca683bb41d5a7706060326ba7
adslyw <adslyw@gmail.com>
2014-08-28 17:41:36 +0800

    add FeeKind model

---
+++ app/models/fee_kind.rb
@@ -0,0 +1,12 @@
+class FeeKind < ActiveRecord::Base
+  attr_accessible :bill_kind, :fee_id, :fee_name, :gather_kind, :call_type, :service_kind
+  establish_connection "remote_db"
+  set_table_name "BF_FEE_KIND_T"
+  set_primary_key :fee_id
+  ignore_table_columns :use_status,
+                       :roam_type,
+                       :toll_type,
+                       :net_type,
+                       :other_flag,
+                       :bill_order
+end
---
+++ test/unit/fee_kind_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FeeKindTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
