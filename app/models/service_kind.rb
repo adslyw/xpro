13db1ec70cfa70eef6a17d52377ec5b9be0fa50e
adslyw <adslyw@gmail.com>
2014-08-28 12:28:43 +0800

    add ServiceKind model

---
+++ app/models/service_kind.rb
@@ -0,0 +1,9 @@
+class ServiceKind < ActiveRecord::Base
+  attr_accessible :service_kind, :service_name
+  establish_connection "remote_db"
+  set_table_name "BS_SERVICE_KIND_T"
+  set_primary_key :service_kind
+  ignore_table_columns :service_flag, :if_open, :month_fee_onoff,
+                       :limit_control, :credit_control, :have_call_limit,
+                       :pay_order
+end
