fad5db41b044ab61b10195c974def19596fccf37
adslyw <adslyw@gmail.com>
2014-11-25 18:05:04 +0800

    change in post view. change in sync model -fix primary_key setting error

--- app/models/sync.rb
+++ app/models/sync.rb
@@ -1,5 +1,5 @@
 class Sync < ActiveRecord::Base
   establish_connection "remote_db"
   attr_accessible :item_name, :item_value
-  self.table_name "SYNC_BILL2BCV_T"
+  self.table_name = "SYNC_BILL2BCV_T"
 end
