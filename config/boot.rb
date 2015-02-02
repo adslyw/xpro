49fbf01edf750fc3aea772222c62a0a4b98cf7cf
adslyw <adslyw@gmail.com>
2015-02-02 18:02:20 +0800

    add unicorn configure set NLS_LANG in boot create bin

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+ENV['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.AL32UTF8'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
